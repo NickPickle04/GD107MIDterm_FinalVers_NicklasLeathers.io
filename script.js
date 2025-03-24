6c6648d251a803365ad68d8deffc01d9fb3ce16f
NickPickle04 <nicklasleathers@gmail.com>
2025-03-23 18:58:40 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('button1').addEventListener('click', function() {
+    window.open('history.html');
+});
+
+
+
+document.getElementById('button2').addEventListener('click', function() {
+    window.open('https://en.wikipedia.org/wiki/The_Starry_Night', '_blank');
+});
+
+
+
+document.getElementById('button3').addEventListener('click', function() {
+    window.open('https://www.britannica.com/technology/television-technology', '_blank');
+});
